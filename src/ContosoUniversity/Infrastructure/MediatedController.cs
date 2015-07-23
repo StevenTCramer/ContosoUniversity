6c3ad4817a04e1a73a0364ede82c1d2d8496ae0b
StevenTCramer <StevenTCramer@.(none)>
2015-07-23 18:02:01 -0400

    Add MediatedController

---
+++ src/ContosoUniversity/Infrastructure/MediatedController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MediatR;
+
+namespace ContosoUniversity.Infrastructure
+{
+	public class MediatedController : Controller
+	{
+		protected readonly IMediator _mediator;
+
+		public MediatedController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+		
+
+		//TODO Can I make this more generic
+		public async Task<ActionResult> Query(IAsyncRequest<IAsyncRequest> query)
+		{
+			var model = await _mediator.SendAsync(query);
+
+			return View(model);
+		}
+
+	}
+}
